3fcdee2f5a9bd062e22327151b450b9330607d4a
Wasinee2516 <71056676+Wasinee2516@users.noreply.github.com>
2021-09-04 15:35:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,105 +4,128 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    
     <title>Show Graph</title>
+    
     <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
   </head>
 
   <body>
-    <h2>62108659 Wasinee Na Ranong</h2>
-    
+
     <div class="container">
-      <div class="class row">
-        <div class="class col-6">
-          <canvas id="myChart" width="400" height="200"></canvas>
-        </div>
-        <div class="class col-6">
-          <canvas id="myChart2" width="400" height="200"></canvas>
-        </div>
-      </div>
-
-      <div class="class row">
-        <div class="class col-3">
-          <div class="class row">
-            <div class="class col-4"><b>Temperature</b></div>
-            <div class="col-8" >
-              <span id="lastTemperature"></span>
-            </div>
-              
-            
-          </div>
-
-          <div class="class row">
-            <div class="class col-4"><b>Humidity</b></div>
-            <div class="col-8" >
-              <span id="lastHumidity"></span>
-            </div>
-              
-            
-          </div>
-
-          <div class="class row">
-            <div class="class col-4"><b>Update</b></div>
-            <div class="col-8" >
-              <span id="lastUpdate"></span>
-            </div>
-              
-            
-          </div>
-        </div>
-      </div>
+        <p><b>62108659 Wasinee Na Ranong</b></p>
     </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <canvas id="showtemp" width="400" height="200"></canvas>
+            </div>
+            <div class="class col-4">
+                <canvas id="showhum" width="400" height="200"></canvas>
+            </div>
+
+            <div class="class col-4">
+                <canvas id="showlight" width="400" height="200"></canvas>
+            </div>
+        </div>
+
+        <div class="class row">
+            <div class="class col-3">
+
+              <div class="class row">
+                <div class="class col-4"><b>Temperature</b></div>
+                <div class="col-8" >
+                  <span id="lastTemperature"></span>
+                </div>
+              </div>
+
+              <div class="class row">
+                <div class="class col-4"><b>Humidity</b></div>
+                <div class="col-8" >
+                  <span id="lastHumidity"></span>
+                </div>           
+              </div>
+
+              <div class="class row">
+                <div class="class col-4"><b>Light</b></div>
+                <div class="col-8" >
+                    <span id="lastLight"></span>
+                </div>
+              </div>
+
+              <div class="class row">
+                <div class="class col-4"><b>Update</b></div>
+                <div class="col-8" >
+                  <span id="lastUpdate"></span>
+                </div>
+              </div>
+            
+            </div>
+        </div>
+    </div>
 
   </body>
+
   <script>
 
-    function showChart(data){
-      var ctx=document.getElementById('myChart').getContext('2d');
-      var myChart=new Chart(ctx,{
-          type:'line',
-          data:{
-            labels:data.xlabel,
-            datasets:[{
-                label:data.label,
-                data:data.data
-            }]
-          }
-      });
-    }
+        function showtemp(data){
+            var ctx=document.getElementById('showtemp').getContext('2d');
+            var showtemp=new Chart(ctx,{
+                type:'line',
+                data:{
+                labels:data.xlabel,
+                datasets:[{
+                    label:data.label,
+                    data:data.data,
+                    fill: false,
+                    borderColor: '#9d43a0 ',
+                    tension: 0.1
+                    }]
+                }
+            });
+        }
 
-    function showLine(data2){
-      var ctxy=document.getElementById('myChart2').getContext('2d');
-      var myChart=new Chart(ctxy,{
-          type:'line',
-          data:{
-            labels:data2.xlabel,
-            datasets:[{
-                label:data2.label,
-                data:data2.data
-            }]
-          }
-      });
-    }
-    function showLine(data2){
-      var ctxy=document.getElementById('myChart2').getContext('2d');
-      var myChart=new Chart(ctxy,{
-          type:'line',
-          data:{
-            labels:data2.xlabel,
-            datasets:[{
-                label:data2.label,
-                data:data2.data
-            }]
-          }
-      });
-    }
-      
-      $(()=>{
+        function showhum(data2){
+            var ctxy=document.getElementById('showhum').getContext('2d');
+            var showhum=new Chart(ctxy,{
+                type:'line',
+                data:{
+                labels:data2.xlabel,
+                datasets:[{
+                    label:data2.label,
+                    data:data2.data,
+                    fill: false,
+                    borderColor: '#9d43a0 ',
+                    tension: 0.1
+                    }]
+                }
+            });
+        }
+        
+        function showlight(data3){
+            var ctxy=document.getElementById('showlight').getContext('2d');
+            var showlight=new Chart(ctxy,{
+                type:'line',
+                data:{
+                labels:data3.xlabel,
+                datasets:[{
+                    label:data3.label,
+                    data:data3.data,
+                    fill: false,
+                    borderColor: '#9d43a0 ',
+                    tension: 0.1
+                    }]
+                }
+            });
+        }
+
+        $(()=>{
           
-          let url="https://api.thingspeak.com/channels/1458415/fields/1.json?results=50";
+          let url = "https://api.thingspeak.com/channels/1458415/feeds.json?results=50";
+
           $.getJSON(url)
             .done(function(data){
               //console.log(data);
@@ -120,11 +143,13 @@
              var xlabel=[];
              var temp=[];
              var humi=[];
+             var ligh=[];
              
              $.each(feed,(k,v)=>{
                 xlabel.push(v.entry_id);
                 humi.push(v.field1);
                 temp.push(v.field2);
+                ligh.push(v.field3);
                 console.log(k,humi);
              });
              
@@ -132,19 +157,26 @@
               data.xlabel=xlabel;
               data.data=temp;
               data.label=chan.field2;
-              showChart(data);
+              showtemp(data);
 
             
               var data2=new Object();
               data2.xlabel=xlabel;
               data2.data=humi;
               data2.label=chan.field1;
-              showLine(data2);
+              showhum(data2);
+
+              var data3=new Object();
+              data3.xlabel=xlabel;
+              data3.data=ligh;
+              data3.label=chan.field3;
+              showlight(data3);
 
 
               
               $("#lastTemperature").text(feed[0].field2+" C");
               $("#lastHumidity").text(feed[0].field1+" %");
+              $("#lastLight").text(feed[0].field3);
               $("#lastUpdate").text(dateStr);
               console.log(humi);
               
@@ -152,6 +184,7 @@
             .fail(function(error){
             });
       });
-    
+
+
   </script>
 </html>
